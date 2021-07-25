5dee5b45d019ee244f7cdc552dcfbb6f0b270ccb
Quentin C <quentin.cousin76400@gmail.com>
2021-07-25 21:33:07 +0000

    Resolve "Rename for scientist name

--- README.md
+++ README.md
@@ -2,18 +2,81 @@
 
 # Requirements
 You must have python 3.8.5 installed on your system
+You must have install all python packages present in requirements.txt
 
 # Script
-You can launch the script compare nucleotide with this command => 
+You will find multiple script on this repo.
 
-`python compare_arn.py`
+`compare_complementary_arn.py`
+This script will compare the arn on complementary method with add space. Example:
 
-OR 
+Sequence1=ACGU
 
-`python3 compare_arn.py`
+Sequence2=GGGC
 
-The script will be create/override a file named result.txt which it contains output of the script.
+AddSpace1=True
+
+For this example, the char `_` represent a ` ` space.
+
+ACGU compare to GGGC
+None
+C can imbricate G
+None
+None
+
+
+
+_ACGU compare to GGGC
+None
+None
+None
+None
+
+
+
+__ACGU compare to GGGC
+None
+None
+None
+G can imbricate C
+
+
+
+`compare_loop_arn.py` 
+
+`generate_permutations_arn.py`
+This script will generate all possible of permutations of arn.
+The file will contains data like this =>
+
+A0C1G2U3
+A0C1U3G2
+
+These values describe nucleotides and original position in the arn.
+
+`compare_position_arn.py`
+This script will just compare the arn positions.
+
+Example:
+
+Sequence1=ACUGU
+Sequence2=UGUAC
+
+Size is same
+A is different of U
+C is different of G
+
+
+# How-to use
+
+To use script you can get help from command line with =>
+`python compare_position_arn.py --help`
+
+`python compare_loop_arn.py --help`
+
+`python compare_complementary_arn.py --help`
+
+`python generate_permutations_arn.py --help`
+
+The script will be create/override a file named result.txt which it contains logs of the script.
 You can see output of the script in standard output (terminal) if you use the `verbose` argument
 
-# Property
-This software is the property of Quentin Cousin. https://gitlab.com/Itarix.
--- compare_position_arn.py
+++ compare_position_arn.py
@@ -1,14 +1,15 @@
 import getopt
 import sys
+from datetime import datetime
+
 import log
 from arn import Arn
-from log import Log
 
 
-def compare_strict_arn(arn1: Arn, arn2: Arn, logger: Log):
+def compare_position_arn(arn1: Arn, arn2: Arn) -> object:
     """
 
-    :rtype: object
+    :rtype: int
     """
     sequence_1 = arn1.get_sequence_str()
     sequence_2 = arn2.get_sequence_str()
@@ -16,14 +17,14 @@ def compare_strict_arn(arn1: Arn, arn2: Arn, logger: Log):
     size_sequence_1 = len(sequence_1)
     size_sequence_2 = len(sequence_2)
 
-    nb_error: int = 0
+    nb_mismatch: int = 0
     for i in range(0, size_sequence_1):
         if size_sequence_1 != size_sequence_2 and (i in (size_sequence_1, size_sequence_2)):
             # logger.warning(f'{sequence_1:26} | {sequence_2:26} ===> '
             #                f'Error: {"Bad size sequences":30} => '
             #                f'sequence val 1 : {sequence_1[i]:1} vs '
             #                f'sequence val 2 : {" ":1} ==> at position {i:10d}')
-            nb_error += 1
+            nb_mismatch += 1
             break
 
         if sequence_1[i] != sequence_2[i]:
@@ -31,15 +32,14 @@ def compare_strict_arn(arn1: Arn, arn2: Arn, logger: Log):
             #                f'Error: {"Bad value":30} =>'
             #                f'sequence val 1 : {sequence_1[i]:1} vs '
             #                f'sequence val 2 : {sequence_2[i]:1} ==> at position {i:10d}')
-            nb_error += 1
-
-    if nb_error > 0:
-        logger.warning(f'{sequence_1:26} | {sequence_2:26} ===> '
-                       f'Number of errors while analyse sequences => nb error {nb_error:2d}')
-
+            nb_mismatch += 1
+    if nb_mismatch > 0:
+        logger.warning(f'{sequence_1:26} | {sequence_2:26} | '
+                       f'Number of mismatch while analyse sequences => nb error {nb_mismatch:2d}')
+    return nb_mismatch
 
 def usage():
-    help_text = """This script is used to compare sequence of nucleotides
+    help_text = """This script is used to compare positions of two arns
         
         This script accept parameters of this list. 
         
@@ -57,10 +57,9 @@ def usage():
             If provided ; it will print in standard output logs.
             If not : it will not print logs.
             Example = --verbose
-        --output
-            Specify the file where the file of result will be create.
-            Warning. The output file can be huge. At least 60GB
-            Example = --output=/tmp/resultFile.txt
+        --log_output
+            Specify the path where the log file will be create.
+            Example = --output=/tmp/log.txt
 """
     print(help_text)
 
@@ -71,7 +70,7 @@ if __name__ == "__main__":
     SEQUENCE_2 = "UAACACUGUCUGGUAACGAUGQ"
     # jf true so it will add space before sequence 2
     # If False so it will add space before sequence 1
-    FILENAME_OUTPUT = ""
+    PATH_LOG = ""
     IS_VERBOSE = False
 
     try:
@@ -80,7 +79,7 @@ if __name__ == "__main__":
             "h",
             [
                 'help', 'verbose',
-                'sequence1=', 'sequence2=', 'output=']
+                'sequence1=', 'sequence2=', 'log_output=']
         )
     except getopt.GetoptError as err:
         usage()
@@ -95,21 +94,26 @@ if __name__ == "__main__":
             SEQUENCE_1 = v
         if k == '--sequence2':
             SEQUENCE_2 = v
-        if k == '--output':
-            FILENAME_OUTPUT = v
+        if k == '--log_output':
+            PATH_LOG = v
         if k == '--verbose':
             IS_VERBOSE = True
 
     if len(SEQUENCE_1) > 30 or len(SEQUENCE_2) > 30:
-        print("Arn sequences max size is 30")
+        print("Error: Arn sequences max size is 30")
         exit(1)
 
-    logger = log.Log(None, IS_VERBOSE, FILENAME_OUTPUT)
+    logger = log.Log(None, IS_VERBOSE, PATH_LOG)
     arn1 = Arn(SEQUENCE_1)
     arn2 = Arn(SEQUENCE_2)
 
+    dateDebut = datetime.now()
+
+    logger.debug("date start : " + str(dateDebut))
     logger.debug("-------------------------------------")
     logger.debug("Check sequences Start.")
-    compare_strict_arn(arn1, arn2, logger)
+    compare_position_arn(arn1, arn2)
     logger.debug("Check sequences End.")
     logger.debug("-------------------------------------")
+    logger.debug("date end : " + str(datetime.now()))
+    logger.debug("process time : " + str(datetime.now() - dateDebut))
